d6f1998b3e067ccbad93820556d5d73eca7a0acf
Tasmia-Chowdhury-Alif <tasmiachowdhuryalif222@gmail.com>
2025-09-14 22:13:02 +0600

    add payment_success url

--- DocEra_Health_api/appointment/urls.py
+++ DocEra_Health_api/appointment/urls.py
@@ -9,4 +9,5 @@ router.register('', views.AppointmentViewset)
 urlpatterns = [
     path('', include(router.urls)),
     path('webhook/', views.AppointmentViewset.as_view({'post': 'stripe_webhook'}), name='stripe_webhook'),
+    path('success/', views.success_view, name='payment_success'),
 ]
