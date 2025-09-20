0e2a2e903be49771ae7e57efdb18f52b8ad4c3c8
Tasmia-Chowdhury-Alif <tasmiachowdhuryalif222@gmail.com>
2025-09-20 11:32:49 +0600

    updated admin class with unfold admin

--- DocEra_Health_api/patient/admin.py
+++ DocEra_Health_api/patient/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
+from unfold.admin import ModelAdmin
 from .models import Patient
 
 # Register your models here.
-class PatientModelAdmin(admin.ModelAdmin):
+class PatientModelAdmin(ModelAdmin):
     list_display = ['first_name', 'last_name', 'mobile_no', 'image']
 
     def first_name(self, obj):
