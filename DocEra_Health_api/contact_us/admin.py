80fdbed96fd8a2b6e8bfad93babd0a1da232a22c
Tasmia-Chowdhury-Alif <tasmiachowdhuryalif222@gmail.com>
2025-09-20 11:32:08 +0600

    updated admin class with unfold admin

--- DocEra_Health_api/contact_us/admin.py
+++ DocEra_Health_api/contact_us/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
+from unfold.admin import ModelAdmin
 from .models import ContactUs
 
 # Register your models here.
-class ContactModelAdmin(admin.ModelAdmin):
+class ContactModelAdmin(ModelAdmin):
     list_display = ['name', 'phone', 'problem']
     
 admin.site.register(ContactUs, ContactModelAdmin)
