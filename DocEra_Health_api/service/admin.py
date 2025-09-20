ecd3d603e94febf193889f180e41b8375c6f1008
Tasmia-Chowdhury-Alif <tasmiachowdhuryalif222@gmail.com>
2025-09-20 11:33:14 +0600

    updated admin class with unfold admin

--- DocEra_Health_api/service/admin.py
+++ DocEra_Health_api/service/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
+from unfold.admin import ModelAdmin
 from .models import Service
 
 # Register your models here.
-admin.site.register(Service)
+admin.site.register(Service, ModelAdmin)
