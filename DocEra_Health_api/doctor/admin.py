65cd57ebcc92b92437a912af1be627eac098cb76
Tasmia-Chowdhury-Alif <tasmiachowdhuryalif222@gmail.com>
2025-09-20 11:32:41 +0600

    updated admin class with unfold admin

--- DocEra_Health_api/doctor/admin.py
+++ DocEra_Health_api/doctor/admin.py
@@ -1,15 +1,16 @@
 from django.contrib import admin
+from unfold.admin import ModelAdmin
 from .models import Doctor, Designation, Specialization, AvailableTime, Review
 
 # Register your models here.
-class DesignationAdmin(admin.ModelAdmin):
+class DesignationAdmin(ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
 
-class SpecializationAdmin(admin.ModelAdmin):
+class SpecializationAdmin(ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
 
-admin.site.register(Doctor)
+admin.site.register(Doctor, ModelAdmin)
 admin.site.register(Designation, DesignationAdmin)
 admin.site.register(Specialization, SpecializationAdmin)
-admin.site.register(AvailableTime)
-admin.site.register(Review)
+admin.site.register(AvailableTime, ModelAdmin)
+admin.site.register(Review, ModelAdmin)
