b66a545f4427c21da2e0cbc167031ca06ddaa98b
Tasmia-Chowdhury-Alif <tasmiachowdhuryalif222@gmail.com>
2025-10-02 23:24:42 +0600

    feat(api-docs): enhanced OpenAPI schema and proper documented code.

--- DocEra_Health_api/patient/models.py
+++ DocEra_Health_api/patient/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
+
 class Patient(models.Model):
     user = models.OneToOneField(User, on_delete= models.CASCADE, related_name='patient')
     image = models.ImageField(upload_to= 'patients/images/', null=True, blank=True)
