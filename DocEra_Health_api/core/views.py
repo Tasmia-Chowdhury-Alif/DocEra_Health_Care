948e84dfedfc46964938a522e242fee152610724
Tasmia-Chowdhury-Alif <tasmiachowdhuryalif222@gmail.com>
2025-10-02 23:30:13 +0600

    feat: Implement api root view

--- DocEra_Health_api/core/views.py
+++ DocEra_Health_api/core/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from drf_spectacular.utils import extend_schema
 
-# Create your views here.
+class ApiRootView(APIView):
+    @extend_schema(exclude=True)  # Excludes from schema
+    def get(self, request, format=None):
+        return Response({
+            'appointment': reverse('appointment:appointment-list', request=request, format=format),
+            'contact_us': reverse('contact_us:contact-list', request=request, format=format),
+            'doctor': reverse('doctor:doctor-list', request=request, format=format),
+            'patient': reverse('patient:patient-list', request=request, format=format),
+            'service': reverse('service:service-list', request=request, format=format),
+        })
