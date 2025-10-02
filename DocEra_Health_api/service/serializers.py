b7c0f2e00889fb1c23e93aaa63e34fdc47b25df7
Tasmia-Chowdhury-Alif <tasmiachowdhuryalif222@gmail.com>
2025-10-02 23:25:09 +0600

    feat(api-docs): enhanced OpenAPI schema and proper documented code.

--- DocEra_Health_api/service/serializers.py
+++ DocEra_Health_api/service/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
 from . import models 
+from drf_spectacular.utils import extend_schema_serializer
 
+
+@extend_schema_serializer()
 class ServiceSerializer(serializers.ModelSerializer):
+    """
+    Serializer for Service CRUD.
+    """
     class Meta:
         model = models.Service
         fields = '__all__'
+        extra_kwargs = {'description': {'help_text': 'Rich text (CKEditor).'}}
