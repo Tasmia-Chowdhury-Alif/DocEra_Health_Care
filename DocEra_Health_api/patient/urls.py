395c5b409c0fb389ec27f562594191ea7cfcd742
Tasmia-Chowdhury-Alif <tasmiachowdhuryalif222@gmail.com>
2025-10-02 23:31:30 +0600

    feat: Implement api root view

--- DocEra_Health_api/patient/urls.py
+++ DocEra_Health_api/patient/urls.py
@@ -2,9 +2,11 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views
 
+app_name = 'patient'
+
 router = DefaultRouter()
 
-router.register('list', views.PatientViewset)
+router.register('', views.PatientViewset, basename='patient')
 
 urlpatterns = [
     path('', include(router.urls)),
